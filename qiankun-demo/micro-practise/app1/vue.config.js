8c6688a7d5a813265c5e510705ec809eb654edb6
wangtian <1434215837@qq.com>
2021-10-25 17:56:34 +0800

    修改了在生产环境下面的微应用的entry配置 和publicPath配置

--- qiankun-demo/micro-practise/app1/vue.config.js
+++ qiankun-demo/micro-practise/app1/vue.config.js
@@ -1,23 +1,23 @@
 let port = 8081;
-const packageName = require("./package.json").name;
-const publicPath = "/";
+const packageName = require('./package.json').name;
+const publicPath = process.env.NODE_ENV === 'production' ? '/app1' : '/';
 module.exports = {
   publicPath,
   devServer: {
     open: true,
     port: port,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+      'Access-Control-Allow-Origin': '*'
+    }
   },
   configureWebpack: {
     output: {
       library: packageName,
-      libraryTarget: "umd",
-      chunkLoadingGlobal: `chunkLoadingGlobal_${packageName}`,
+      libraryTarget: 'umd',
+      chunkLoadingGlobal: `chunkLoadingGlobal_${packageName}`
     },
     externals: {
-      "vue-router": "VueRouter",
-    },
-  },
+      'vue-router': 'VueRouter'
+    }
+  }
 };
--- qiankun-demo/micro-practise/app2/vue.config.js
+++ qiankun-demo/micro-practise/app2/vue.config.js
@@ -1,20 +1,20 @@
 let port = 8082;
-const packageName = require("./package.json").name;
-const publicPath = "/";
+const packageName = require('./package.json').name;
+const publicPath = process.env.NODE_ENV === 'production' ? '/app2' : '/';
 module.exports = {
   publicPath,
   devServer: {
     open: true,
     port: port,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+      'Access-Control-Allow-Origin': '*'
+    }
   },
   configureWebpack: {
     output: {
       library: packageName,
-      libraryTarget: "umd",
-      chunkLoadingGlobal: `chunkLoadingGlobal_${packageName}`,
-    },
-  },
+      libraryTarget: 'umd',
+      chunkLoadingGlobal: `chunkLoadingGlobal_${packageName}`
+    }
+  }
 };
