d3aaf44a2d78ec057da603c152c676fae4cc2e75
wangtian <1434215837@qq.com>
2021-10-26 17:24:04 +0800

    添加了external的注释

--- qiankun-demo/micro-practise/app1/vue.config.js
+++ qiankun-demo/micro-practise/app1/vue.config.js
@@ -17,6 +17,7 @@ module.exports = {
       chunkLoadingGlobal: `chunkLoadingGlobal_${packageName}`
     },
     externals: {
+      // 在使用import vuerouter from 'vue-router' 时候，不打包vue-router 这个包；并且去找全局的VueRouter 变量
       'vue-router': 'VueRouter'
     }
   }
